956cb8e4c580a3a6507576d1dc7cac03caa68110
stefantanasa <liketanasa@gmail.com>
2022-03-03 19:07:32 +0000

    update

--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -1,19 +1,22 @@
-import fs from "fs-extra" // 3RD PARTY MODULE
-import { fileURLToPath } from "url"
-import { dirname, join } from "path"
+import fs from "fs-extra"; // 3RD PARTY MODULE
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
-const { readJSON, writeJSON, writeFile } = fs
+const { readJSON, writeJSON, writeFile } = fs;
 
-const getJSONPath = filename => join(join(dirname(fileURLToPath(import.meta.url)), "../data"), filename)
+const getJSONPath = (filename) =>
+  join(join(dirname(fileURLToPath(import.meta.url)), "../data"), filename);
 
-const booksJSONPath = getJSONPath("books.json")
-const usersJSONPath = getJSONPath("users.json")
+const booksJSONPath = getJSONPath("books.json");
+const usersJSONPath = getJSONPath("users.json");
 
-const usersPublicFolderPath = join(process.cwd(), "./public/img/users")
+const usersPublicFolderPath = join(process.cwd(), "./public/img/users");
 
-export const getBooks = () => readJSON(booksJSONPath)
-export const writeBooks = content => writeJSON(booksJSONPath, content)
-export const getUsers = () => readJSON(usersJSONPath)
-export const writeUsers = content => writeJSON(usersJSONPath, content)
+export const getBooks = () => readJSON(booksJSONPath);
+export const writeBooks = (content) => writeJSON(booksJSONPath, content);
+export const getUsers = () => readJSON(usersJSONPath);
+export const writeUsers = (content) => writeJSON(usersJSONPath, content);
 
-export const saveUsersPictures = (filename, contentAsABuffer) => writeFile(join(usersPublicFolderPath, filename), contentAsABuffer)
+export const saveUsersPictures = (filename, contentAsABuffer) => {
+  writeFile(join(usersPublicFolderPath, filename), contentAsABuffer);
+};
